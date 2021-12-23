806f91d4c1c3635c0c954bfd6693a3226de25658
kyorohiro <kyorohiro@gmail.com>
2021-12-24 01:41:00 +0900

    ..

--- test/dart2_test.dart
+++ test/dart2_test.dart
@@ -6,7 +6,7 @@ void main() {
     setUp(() {});
 
     test('DNS.urlToQname', () {
-      var out = DNS.urlToQname("github.com");
+      var out = DNS.urlToQname('github.com');
       var ret = [6, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 3, 0x63, 0x6f, 0x6d, 0x00];
       expect(out.length, ret.length);
       expect(out, ret);
