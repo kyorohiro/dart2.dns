8a3c70cb5fc7e60cf463a9116659a320f55f0a3b
kyorohiro <kyorohiro@gmail.com>
2021-12-24 22:28:59 +0900

    ..

--- example/dart2_example.dart
+++ example/dart2_example.dart
@@ -7,6 +7,7 @@ void main() {
   var buffer = DNS().generateAMessage("github.com");
   //Buffer buffer = new Buffer(54);
   //buffer.setInt16AtBigEndian(0, 123);
-  buffer.printAtHex();
+  //buffer.printAtHex();
+  print(buffer.toBase64());
 }
 //007b0100000100000000000005716969746103636f6d0000010001
