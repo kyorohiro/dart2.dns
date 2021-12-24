ad3e259bf1e9664d127fe68a98a1cb450cc2b44c
kyorohiro <kyorohiro@gmail.com>
2021-12-24 18:09:43 +0900

    ..

--- lib/dns.dart
+++ lib/dns.dart
@@ -6,6 +6,68 @@ import 'dart:typed_data' show Uint8List;
 import 'package:tuple/tuple.dart' show Tuple2;
 export 'src/buffer.dart';
 
+///
+/// https://datatracker.ietf.org/doc/html/rfc1035
+///
+class DNSHeader {
+  //
+  int qr = 0; // QR:  1bit  query (0), or a response (1).
+  int opcode = DNS.OPCODE_QUERY; // OPCODE: 4bit
+  bool aa = false; // AA: 1bit
+  bool tc = false; // TC: 1bit
+  bool rd = true; // RD: 1bit
+  bool ra = false; // RA: 1bit
+  int z = 0; // Z: 3bit
+  int rcode = DNS.RCODE_NO_ERROR; // RCODE: 4bit
+  int qdcount = 1; // QDCOUNT: 16bit
+  int ancount = 0; // ANCOUNT: 16bit
+  int nscount = 0; // NSCOUNT: 16bit
+  int arcount = 0; // ARCOUNT: 16bit
+
+  Buffer generate() {
+    var buffer = Buffer(12);
+
+    ///
+    /// HEADER
+    ///
+    {
+      // ID: 16bit
+      buffer.setInt16AtBigEndian(0, 123);
+    }
+    {
+      var tmp = 0x00;
+      tmp |= (qr << 7) & 0xFF;
+      tmp |= (opcode << 3);
+      if (aa) {
+        tmp |= (0x01 << 2) & 0xFF;
+      }
+      if (tc) {
+        tmp |= (0x01 << 1) & 0xFF;
+      }
+      if (rd) {
+        tmp |= (0x01 << 0) & 0xFF;
+      }
+      buffer.setByteAtBigEndian(2, tmp);
+    }
+    {
+      var tmp = 0x00;
+      if (ra) {
+        tmp |= (0x01 << 7) & 0xFF;
+      }
+      tmp |= (z << 4) & 0xFF;
+      tmp |= (rcode) & 0xFF;
+
+      buffer.setByteAtBigEndian(3, tmp);
+    }
+
+    buffer.setInt16AtBigEndian(4, qdcount);
+    buffer.setInt16AtBigEndian(6, ancount);
+    buffer.setInt16AtBigEndian(8, nscount);
+    buffer.setInt16AtBigEndian(10, arcount);
+    return buffer;
+  }
+}
+
 class DNS {
   static final int OPCODE_QUERY = 0;
   static final int OPCPDE_IQUERY = 1;
@@ -17,6 +79,23 @@ class DNS {
   static final int RCODE_NAME_ERROR = 3;
   static final int RCODE_NOT_IMPLEMENTED = 4;
   static final int RCODE_REFUSED = 5;
+
+  static final int QTYPE_A = 1; // a host address
+  static final int QTYPE_NS = 2; // an authoritative name server
+  static final int QTYPE_MD = 3; // a mail destination (Obsolete - use MX)
+  static final int QTYPE_MF = 4; // a mail forwarder (Obsolete - use MX)
+  static final int QTYPE_CNAME = 5; // the canonical name for an alias
+  static final int QTYPE_SOA = 6; // marks the start of a zone of authority
+  static final int QTYPE_MB = 7; // a mailbox domain name (EXPERIMENTAL)
+  static final int QTYPE_MG = 8; // a mail group member (EXPERIMENTAL)
+  static final int QTYPE_MR = 9; // a mail rename domain name (EXPERIMENTAL)
+  static final int QTYPE_NULL = 10; // a null RR (EXPERIMENTAL)
+  static final int QTYPE_WKS = 11; // a well known service description
+  static final int QTYPE_PTR = 12; // a domain name pointer
+  static final int QTYPE_HINFO = 13; // host information
+  static final int QTYPE_MINFO = 14; // mailbox or mail list information
+  static final int QTYPE_MX = 15; // mail exchange
+  static final int QTYPE_TXT = 16; // text strings
 
   static Uint8List urlToQname(String url) {
     var urlBytes = ascii.encode(url);
