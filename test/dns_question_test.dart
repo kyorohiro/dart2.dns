84cc7f0b625c99a8f8ad9d9c36ca7300978796f2
kyorohiro <kyorohiro@gmail.com>
2021-12-25 16:42:17 +0900

    add dnsquestion decode

--- test/dns_question_test.dart
+++ test/dns_question_test.dart
@@ -19,25 +19,6 @@ void main() {
       expect(question.item1[0].qType, 1);
     });
 
-    ///
-    /// 1: 06
-    /// 2: 67
-    /// 3: 69
-    /// 4: 74
-    /// 5: 68
-    /// 6: 75
-    /// 7: 62
-    /// 8: 03
-    /// 9: 63
-    /// 10: 6f
-    /// 11: 6d
-    /// 12: 00
-    /// 13: 00
-    /// 14: 01
-    /// 15: 00
-    /// 16: 01
-    ///
-
     test('DNSQuestion.decode() 1', () {
       var buffer1 = DNSQuestion.encode(DNSQuestion()..hostOrIP = 'github.com');
       var buffer2 = DNSQuestion.encode(DNSQuestion()..hostOrIP = 'yahoo.co.jp');
@@ -48,9 +29,5 @@ void main() {
       expect(question.item1[0].hostOrIP, 'github.com');
       expect(question.item1[1].hostOrIP, 'yahoo.co.jp');
     });
-
-    // var buffer = DNSQuestion.encode(DNSQuestion()..hostOrIP = 'github.com')
-    //0667697468756203636f6d0000010001
-    //057961686f6f02636f026a700000010001
   });
 }
